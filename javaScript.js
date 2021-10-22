c7c02b1a2eafc85d972c6a686603dca7f9661310
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-23 00:00:44 +0600

    hello

--- javaScript.js
+++ javaScript.js
@@ -109,3 +109,18 @@
 
 // const date = new Date();
 // console.log(date);
+
+// check if the number is positive or negative/zero
+
+const number = prompt("Enter a number: ");
+
+// check if number is greater than 0
+if (number > 0) {
+  console.log("The number is positive");
+}
+// if number is not greater than 0
+else {
+  console.log("The number is either a negative number or 0");
+}
+
+console.log("The if...else statement is easy");
