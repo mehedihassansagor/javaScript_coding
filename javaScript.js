bdf70cca08458cb9692aefa8174bd11164a66102
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-23 00:08:56 +0600

    hello

--- javaScript.js
+++ javaScript.js
@@ -117,7 +117,7 @@ if (number > 0) {
   console.log("The number is positive.")
 }
 else {
-  console.log("The number is either a negative number or 0...")
+  console.log("The number is either a negative number or 0..")
 }
 
 console.log("The if.....else statement is easy")
