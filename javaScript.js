0a07a18867291c28b1ae9c4e27912f527b5f2fa1
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-23 00:02:13 +0600

    hello

--- javaScript.js
+++ javaScript.js
@@ -112,7 +112,7 @@
 
 // check if the number is positive or negative/zero
 
-const number = prompt("Enter a number: ");
+const number = prompt("Enter a number: ")
 
 // check if number is greater than 0
 if (number > 0) {
