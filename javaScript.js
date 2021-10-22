b235313f8313fb72349825e9c35b471b0c0b6892
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-22 21:44:00 +0600

    adding

--- javaScript.js
+++ javaScript.js
@@ -1,23 +1,43 @@
 //prime number
-function isPrime(num) {
+// function isPrime(num) {
 
-    if (num === 2) {
-      return true;
-    } else if (num > 1) {
-      for (var i = 2; i < num; i++) {
+//     if (num === 2) {
+//       return true;
+//     } else if (num > 1) {
+//       for (var i = 2; i < num; i++) {
   
-        if (num % i !== 0) {
-          return true;
-        } else if (num === i * i) {
-          return false
-        } else {
-          return false;
-        }
-      }
-    } else {
-      return false;
-    }
+//         if (num % i !== 0) {
+//           return true;
+//         } else if (num === i * i) {
+//           return false
+//         } else {
+//           return false;
+//         }
+//       }
+//     } else {
+//       return false;
+//     }
   
-  }
+//   }
   
-  console.log(isPrime(6))
+//   console.log(isPrime(6))
+
+//factroial using recursion
+// var number = 2
+// if (number < 0) {
+//     console.log('Error');
+// }
+
+// // if number is 0
+// else if (number === 0) {
+//     console.log(`The factorial of ${number} is 1.`);
+// }
+
+// // if number is positive
+// else {
+//     let fact = 1;
+//     for (i = 1; i <= number; i++) {
+//         fact *= i;
+//     }
+//     console.log(fact);
+// }
