d889cebaa3cb227c25131aedf77f199659f497ef
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-22 21:54:50 +0600

    function

--- javaScript.js
+++ javaScript.js
@@ -52,3 +52,17 @@
 // console.log(leapyear(1700));
 // console.log(leapyear(1800));
 // console.log(leapyear(100));
+
+// var myObject = {
+//     foo: "bar",
+//     func: function() {
+//         var self = this;
+//         console.log("outer func:  this.foo = " + this.foo);
+//         console.log("outer func:  self.foo = " + self.foo);
+//         (function() {
+//             console.log("inner func:  this.foo = " + this.foo);
+//             console.log("inner func:  self.foo = " + self.foo);
+//         }());
+//     }
+// };
+// myObject.func();
