16de156dc633c41275e1f9b18cf7cb6d982b1d5e
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-22 21:55:34 +0600

    function

--- javaScript.js
+++ javaScript.js
@@ -66,3 +66,18 @@
 //     }
 // };
 // myObject.func();
+
+// function foo1()
+// {
+//   return {
+//       bar: "hello"
+//   };
+// }
+
+// function foo2()
+// {
+//   return
+//   {
+//       bar: "hello"
+//   };
+// }
