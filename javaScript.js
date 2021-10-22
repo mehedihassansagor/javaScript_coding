49df987176669bbe58f2699b50491c3228a3665f
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-22 21:49:50 +0600

    update

--- javaScript.js
+++ javaScript.js
@@ -42,12 +42,13 @@
 //     console.log(fact);
 // }
 
-function leapyear(year)
-{
-return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
-}
-console.log(leapyear(2016));
-console.log(leapyear(2000));
-console.log(leapyear(1700));
-console.log(leapyear(1800));
-console.log(leapyear(100));
+//leap year
+// function leapyear(year)
+// {
+// return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+// }
+// console.log(leapyear(2016));
+// console.log(leapyear(2000));
+// console.log(leapyear(1700));
+// console.log(leapyear(1800));
+// console.log(leapyear(100));
