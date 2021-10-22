573b0e96f2fc99a9fcb54b6719e2d66ce8574dbc
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-22 21:40:12 +0600

    practse

---
+++ javaScript.js
@@ -0,0 +1,23 @@
+//prime number
+function isPrime(num) {
+
+    if (num === 2) {
+      return true;
+    } else if (num > 1) {
+      for (var i = 2; i < num; i++) {
+  
+        if (num % i !== 0) {
+          return true;
+        } else if (num === i * i) {
+          return false
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  
+  }
+  
+  console.log(isPrime(6))
