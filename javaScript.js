24841b101c97ef750161d7a0c9a667aaaef47e1b
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-22 21:56:32 +0600

    iffe

--- javaScript.js
+++ javaScript.js
@@ -81,3 +81,12 @@
 //       bar: "hello"
 //   };
 // }
+
+//iffi function
+
+// (function() {
+//     console.log(1); 
+//     setTimeout(function(){console.log(2)}, 1000); 
+//     setTimeout(function(){console.log(3)}, 0); 
+//     console.log(4);
+// })();
