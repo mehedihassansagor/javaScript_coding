4e4a5a68eb63e749dc2081e5f0c2b81ec6308f5d
SAGOR <seakingmawmaw2020@gmail.com>
2021-10-23 00:09:12 +0600

    hello

--- javaScript.js
+++ javaScript.js
@@ -114,7 +114,7 @@
 
 const number = prompt("Enter a number..")
 if (number > 0) {
-  console.log("The number is positive.")
+  console.log("The number is positive....")
 }
 else {
   console.log("The number is either a negative number or 0..")
